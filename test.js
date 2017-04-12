59762584152d9f84ee2e6d147401a7d1b715740a
Adrian Unger <dev@staydecent.ca>
2017-04-12 15:20:16 -0700

    Add Inferno test.

--- test.js
+++ test.js
@@ -8,6 +8,11 @@ import render from 'preact-render-to-string'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
 
+// Inferno
+import InfernoComponent from 'inferno-component'
+import infernoCreateElement from 'inferno-create-element'
+import InfernoServer from 'inferno-server'
+
 // Our library!
 import createComponent from './index.js'
 
@@ -41,5 +46,11 @@ describe('createComponent', () => {
     const output = shallowRenderer.getRenderOutput()
     expect(output.type).to.equal('div')
     expect(output.props.children).to.equal('Hello world')
+  })
+
+  it('Should create an Inferno component', () => {
+    const Test = createComponent(InfernoComponent, objWithRender(infernoCreateElement))
+    const output = InfernoServer.renderToString(infernoCreateElement(Test))
+    expect(output).to.equal('<div>Hello world</div>')
   })
 })
