2064b10015f8c239609f3f658deaf4cab28c7567
Adrian Unger <dev@staydecent.ca>
2017-04-13 12:37:43 -0700

    Return a HoC that renders a pure function.
    This way we avoid mutating props on the returned component, which is not allowed by React.

--- index.js
+++ index.js
@@ -5,45 +5,70 @@ const toType = function (val) {
 
 /**
  * Create a class-based Component out of object literals
- * @param  {function} Component React, Preact, Inferno Component creator
- * @param  {object}   objs      object-literals containing lifecycle methods etc.
- * @return {Component}          The final Component to render
+ * @param  {Function} Component   React, Preact, Inferno Component creator
+ * @param  {Function} hyperscript React, Preact, Inferno element/vnode creator
+ * @param  {Object}   objs        object-literals containing lifecycle methods etc.
+ * @return {Component}            The final Component to render
  */
 export function compose () {
   const args = Array.prototype.slice.call(arguments)
   const Component = args[0]
-  const objs = args.slice(1).map((obj) =>
+  const h = args[1]
+
+  if (toType(Component) !== 'function' || toType(h) !== 'function') {
+    throw new Error('compose expects to be called like, `compose(Component, createElement, [{}, {}, ...]`')
+  }
+
+  // Allow partial appliction for reuse, ex:
+  // `const preactCompose = compose(Component, h)`
+  // `preactCompose(withState(..), {}, ...)`
+  if (args.length <= 2) {
+    return compose.bind(null, Component, h)
+  }
+
+  const objs = args.slice(2).map((obj) =>
     toType(obj) === 'function' ? {[obj.name]: obj} : obj
   )
   const obj = Object.assign.apply(Object, [{}].concat(objs))
+  const userRender = obj.render.bind(null)
+  const pfc = function (props) {
+    let newProps = Object.assign({}, props)
 
-  function classlessComponent () {
-    const args = Array.prototype.slice.call(arguments)
-
-    // Return new props, instead if requiring R.pipe or similar
+    // handle mapProps
     if (obj._mapProps) {
-      const newProps = obj._mapProps.apply(this, args)
-      args[0] = Object.assign(args[0], newProps)
+      Object.assign(newProps, obj._mapProps.call(this, props))
       delete obj._mapProps
     }
 
+    // handle withState
     if (obj._mergeState) {
       // Pass props to _initialValue function to set initialValue for withState
       if (obj._initialValue && obj._initialValue.length === 2) {
-        Object.assign(obj.state, {[obj._initialValue[0]]: obj._initialValue[1].apply(null, args)})
+        Object.assign(obj.state, {[obj._initialValue[0]]: obj._initialValue[1].call(null, newProps)})
         delete obj._initialValue
       }
 
       // Bind withState setter and assign state to props
       const setter = obj[obj._mergeState].bind(this)
-      args[0] = Object.assign(args[0], obj.state, {[obj._mergeState]: setter})
+      Object.assign(newProps, obj.state, {[obj._mergeState]: setter})
       delete obj._mergeState
     }
 
-    Component.apply(this, args)
+    const node = userRender(newProps)
+    return node
+  }
 
+  obj.render = function () {
+    return h(pfc, this.props)
+  }
+
+  // Create a HoC class, avoiding class syntax
+  function hoc () {
+    Component.apply(this, arguments)
+
+    // auto-bind methods to the component
     for (let i in obj) {
-      if (i !== 'render' && typeof obj[i] === 'function') {
+      if (i !== 'render' && toType(obj[i]) === 'function') {
         this[i] = obj[i].bind(this)
       }
     }
@@ -53,13 +78,13 @@ export function compose () {
     }
   }
 
-  classlessComponent.prototype = Object.assign(
+  hoc.prototype = Object.assign(
     Object.create(Component.prototype), obj
   )
 
-  classlessComponent.prototype.constructor = classlessComponent
+  hoc.prototype.constructor = hoc
 
-  return classlessComponent
+  return hoc
 }
 
 /**
--- test.js
+++ test.js
@@ -6,7 +6,7 @@ import render from 'preact-render-to-string'
 
 // React
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import ReactTestRenderer from 'react-test-renderer'
 
 // Inferno
 import InfernoComponent from 'inferno-component'
@@ -32,24 +32,21 @@ describe('compose', () => {
   })
 
   it('Should create a Preact component', () => {
-    const Test = compose(Component, objWithRender(h))
-    const output = render(h(Test))
+    const Test = compose(Component, h, objWithRender(h))
+    const output = render(h(Test, {test: 56}))
     expect(output).to.equal('<div>Hello world</div>')
   })
 
   it('Should create a React component', () => {
-    const Test = compose(React.Component, objWithRender(React.createElement))
-
-    const shallowRenderer = new ReactShallowRenderer()
-    shallowRenderer.render(React.createElement(Test))
-
-    const output = shallowRenderer.getRenderOutput()
+    const Test = compose(React.Component, React.createElement, objWithRender(React.createElement))
+    const renderer = ReactTestRenderer.create(React.createElement(Test, {}))
+    const output = renderer.toJSON()
     expect(output.type).to.equal('div')
-    expect(output.props.children).to.equal('Hello world')
+    expect(output.children[0]).to.equal('Hello world')
   })
 
   it('Should create an Inferno component', () => {
-    const Test = compose(InfernoComponent, objWithRender(infernoCreateElement))
+    const Test = compose(InfernoComponent, infernoCreateElement, objWithRender(infernoCreateElement))
     const output = InfernoServer.renderToString(infernoCreateElement(Test))
     expect(output).to.equal('<div>Hello world</div>')
   })
@@ -57,7 +54,7 @@ describe('compose', () => {
 
 describe('withState', () => {
   it('Should set state prop and pass to render', () => {
-    const Counter = compose(Component,
+    const Counter = compose(Component, h,
       withState('counter', 'setCounter', 0),
       function render ({counter, setCounter}) {
         return h('div', null, [
@@ -72,7 +69,7 @@ describe('withState', () => {
   })
 
   it('Should pass props to initialValue function', () => {
-    const Counter = compose(Component,
+    const Counter = compose(Component, h,
       withState('counter', 'setCounter', ({count}) => count),
       function render ({counter, setCounter}) {
         return h('div', null, [
@@ -85,11 +82,25 @@ describe('withState', () => {
     const output = render(h(Counter, {count: 2}))
     expect(output).to.equal('<div>Count: 2<button>Increment</button><button>Decrement</button></div>')
   })
+
+  it('Should work with React', () => {
+    const Counter = compose(React.Component, React.createElement,
+      withState('counter', 'setCounter', ({count}) => count),
+      function render ({counter, setCounter}) {
+        return React.createElement('div', null, ['Count: ' + counter])
+      }
+    )
+    const renderer = ReactTestRenderer.create(
+      React.createElement(Counter, {count: 5}))
+    const output = renderer.toJSON()
+    expect(output.type).to.equal('div')
+    expect(output.children[0]).to.equal('Count: 5')
+  })
 })
 
 describe('mapProps', () => {
-  it('Should work', () => {
-    const List = compose(Component,
+  it('Should work with Preact', () => {
+    const List = compose(Component, h,
       mapProps(({nums}) => ({nums: nums.map((n) => n + 1)})),
       function render ({nums}) {
         return h('div', null, [
@@ -99,5 +110,21 @@ describe('mapProps', () => {
     )
     const output = render(h(List, {nums: [1, 2, 3, 4, 5]}))
     expect(output).to.equal('<div>List: 2, 3, 4, 5, 6</div>')
+  })
+
+  it('Should work with React', () => {
+    const List = compose(React.Component, React.createElement,
+      mapProps((props) => ({nums: props.nums ? props.nums.map((n) => n + 1) : []})),
+      function render ({nums}) {
+        return React.createElement('div', null, [
+          'List: ' + (nums && nums.join(', '))
+        ])
+      }
+    )
+    const renderer = ReactTestRenderer.create(
+      React.createElement(List, {nums: [1, 2, 3, 4, 5]}))
+    const output = renderer.toJSON()
+    expect(output.type).to.equal('div')
+    expect(output.children[0]).to.equal('List: 2, 3, 4, 5, 6')
   })
 })
