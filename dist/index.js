a4e7a2bfa1545521e513831c818ff9aaa39764b2
Adrian Unger <dev@staydecent.ca>
2017-04-12 19:43:32 -0700

    Use module exports

--- dist/index.js
+++ dist/index.js
@@ -2,90 +2,73 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-/* globals define */
-(function (root, factory) {
-  'use strict';
+var toType = function toType(val) {
+  var str = {}.toString.call(val);
+  return str.toLowerCase().slice(8, -1);
+};
 
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object') {
-    module.exports = factory();
-  } else {
-    root.atom = factory();
-  }
-})(undefined, function () {
-  'use strict';
+module.exports = {
+  compose: compose,
+  withState: withState
+};
 
-  var toType = function toType(val) {
-    var str = {}.toString.call(val);
-    return str.toLowerCase().slice(8, -1);
-  };
+/**
+ * Create a class-based Component out of object literals
+ * @param  {function} Component React, Preact, Inferno Component creator
+ * @param  {object}   objs      object-literals containing lifecycle methods etc.
+ * @return {Component}          The final Component to render
+ */
+function compose() {
+  var args = Array.prototype.slice.call(arguments);
+  var Component = args[0];
+  var objs = args.slice(1).map(function (obj) {
+    return toType(obj) === 'function' ? _defineProperty({}, obj.name, obj) : obj;
+  });
+  var obj = Object.assign.apply(Object, [{}].concat(objs));
 
-  return {
-    compose: compose,
-    withState: withState
-  };
-
-  /**
-   * Create a class-based Component out of object literals
-   * @param  {function} Component React, Preact, Inferno Component creator
-   * @param  {object}   objs      object-literals containing lifecycle methods etc.
-   * @return {Component}          The final Component to render
-   */
-  function compose() {
+  function classlessComponent() {
     var args = Array.prototype.slice.call(arguments);
-    var Component = args[0];
-    var objs = args.slice(1).map(function (obj) {
-      return toType(obj) === 'function' ? _defineProperty({}, obj.name, obj) : obj;
-    });
-    var obj = Object.assign.apply(Object, [{}].concat(objs));
+    if (obj._mergeState && obj.state) {
+      var setter = obj[obj._mergeState].bind(this);
+      args[0] = _extends(args[0], obj.state, _defineProperty({}, obj._mergeState, setter));
+      delete obj._mergeState;
+    }
 
-    function classlessComponent() {
-      var args = Array.prototype.slice.call(arguments);
-      if (obj._mergeState && obj.state) {
-        var setter = obj[obj._mergeState].bind(this);
-        args[0] = _extends(args[0], obj.state, _defineProperty({}, obj._mergeState, setter));
-        delete obj._mergeState;
-      }
+    Component.apply(this, args);
 
-      Component.apply(this, args);
-
-      for (var i in obj) {
-        if (i !== 'render' && typeof obj[i] === 'function') {
-          this[i] = obj[i].bind(this);
-        }
-      }
-
-      if (obj.init) {
-        obj.init.call(this);
+    for (var i in obj) {
+      if (i !== 'render' && typeof obj[i] === 'function') {
+        this[i] = obj[i].bind(this);
       }
     }
 
-    classlessComponent.prototype = _extends(Object.create(Component.prototype), obj);
-
-    classlessComponent.prototype.constructor = classlessComponent;
-
-    return classlessComponent;
+    if (obj.init) {
+      obj.init.call(this);
+    }
   }
 
-  /**
-   * Set a state value, and a function to update that state value.
-   * @param  {[type]} propName      [description]
-   * @param  {[type]} setterName    [description]
-   * @param  {[type]} initialValue) [description]
-   * @return {[type]}               [description]
-   */
-  function withState(propName, setterName, initialValue) {
-    var _ref2;
+  classlessComponent.prototype = _extends(Object.create(Component.prototype), obj);
 
-    return _ref2 = {
-      state: _defineProperty({}, propName, initialValue)
-    }, _defineProperty(_ref2, setterName, function setter(val) {
-      this.setState(_defineProperty({}, propName, val));
-    }), _defineProperty(_ref2, '_mergeState', setterName), _ref2;
-  }
-});
+  classlessComponent.prototype.constructor = classlessComponent;
+
+  return classlessComponent;
+}
+
+/**
+ * Set a state value, and a function to update that state value.
+ * @param  {[type]} propName      [description]
+ * @param  {[type]} setterName    [description]
+ * @param  {[type]} initialValue) [description]
+ * @return {[type]}               [description]
+ */
+function withState(propName, setterName, initialValue) {
+  var _ref2;
+
+  return _ref2 = {
+    state: _defineProperty({}, propName, initialValue)
+  }, _defineProperty(_ref2, setterName, function setter(val) {
+    this.setState(_defineProperty({}, propName, val));
+  }), _defineProperty(_ref2, '_mergeState', setterName), _ref2;
+}
--- index.js
+++ index.js
@@ -1,84 +1,70 @@
-/* globals define */
-(function (root, factory) {
-  'use strict'
-  if (typeof define === 'function' && define.amd) {
-    define([], factory)
-  } else if (typeof exports === 'object') {
-    module.exports = factory()
-  } else {
-    root.atom = factory()
-  }
-}(this, function () {
-  'use strict'
+const toType = function (val) {
+  var str = ({}).toString.call(val)
+  return str.toLowerCase().slice(8, -1)
+}
 
-  const toType = function (val) {
-    var str = ({}).toString.call(val)
-    return str.toLowerCase().slice(8, -1)
-  }
+module.exports = {
+  compose,
+  withState
+}
 
-  return {
-    compose,
-    withState
-  }
+/**
+ * Create a class-based Component out of object literals
+ * @param  {function} Component React, Preact, Inferno Component creator
+ * @param  {object}   objs      object-literals containing lifecycle methods etc.
+ * @return {Component}          The final Component to render
+ */
+function compose () {
+  const args = Array.prototype.slice.call(arguments)
+  const Component = args[0]
+  const objs = args.slice(1).map((obj) =>
+    toType(obj) === 'function' ? {[obj.name]: obj} : obj
+  )
+  const obj = Object.assign.apply(Object, [{}].concat(objs))
 
-  /**
-   * Create a class-based Component out of object literals
-   * @param  {function} Component React, Preact, Inferno Component creator
-   * @param  {object}   objs      object-literals containing lifecycle methods etc.
-   * @return {Component}          The final Component to render
-   */
-  function compose () {
+  function classlessComponent () {
     const args = Array.prototype.slice.call(arguments)
-    const Component = args[0]
-    const objs = args.slice(1).map((obj) =>
-      toType(obj) === 'function' ? {[obj.name]: obj} : obj
-    )
-    const obj = Object.assign.apply(Object, [{}].concat(objs))
+    if (obj._mergeState && obj.state) {
+      const setter = obj[obj._mergeState].bind(this)
+      args[0] = Object.assign(args[0], obj.state, {[obj._mergeState]: setter})
+      delete obj._mergeState
+    }
 
-    function classlessComponent () {
-      const args = Array.prototype.slice.call(arguments)
-      if (obj._mergeState && obj.state) {
-        const setter = obj[obj._mergeState].bind(this)
-        args[0] = Object.assign(args[0], obj.state, {[obj._mergeState]: setter})
-        delete obj._mergeState
-      }
+    Component.apply(this, args)
 
-      Component.apply(this, args)
-
-      for (let i in obj) {
-        if (i !== 'render' && typeof obj[i] === 'function') {
-          this[i] = obj[i].bind(this)
-        }
-      }
-
-      if (obj.init) {
-        obj.init.call(this)
+    for (let i in obj) {
+      if (i !== 'render' && typeof obj[i] === 'function') {
+        this[i] = obj[i].bind(this)
       }
     }
 
-    classlessComponent.prototype = Object.assign(
-      Object.create(Component.prototype), obj
-    )
-
-    classlessComponent.prototype.constructor = classlessComponent
-
-    return classlessComponent
-  }
-
-  /**
-   * Set a state value, and a function to update that state value.
-   * @param  {[type]} propName      [description]
-   * @param  {[type]} setterName    [description]
-   * @param  {[type]} initialValue) [description]
-   * @return {[type]}               [description]
-   */
-  function withState (propName, setterName, initialValue) {
-    return {
-      state: {[propName]: initialValue},
-      [setterName]: function setter (val) {
-        this.setState({[propName]: val})
-      },
-      _mergeState: setterName
+    if (obj.init) {
+      obj.init.call(this)
     }
   }
-}))
+
+  classlessComponent.prototype = Object.assign(
+    Object.create(Component.prototype), obj
+  )
+
+  classlessComponent.prototype.constructor = classlessComponent
+
+  return classlessComponent
+}
+
+/**
+ * Set a state value, and a function to update that state value.
+ * @param  {[type]} propName      [description]
+ * @param  {[type]} setterName    [description]
+ * @param  {[type]} initialValue) [description]
+ * @return {[type]}               [description]
+ */
+function withState (propName, setterName, initialValue) {
+  return {
+    state: {[propName]: initialValue},
+    [setterName]: function setter (val) {
+      this.setState({[propName]: val})
+    },
+    _mergeState: setterName
+  }
+}
