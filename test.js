ddc74f2b4ac81637efd9b63f82139c67d988e37f
Adrian Unger <dev@staydecent.ca>
2018-07-17 19:39:33 -0700

    Update to use microbundle and ava.

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/* globals describe it */
+import test from 'ava'
 
 // Preact
 import {Component, h} from 'preact'
@@ -14,11 +14,7 @@ import {createElement as infernoCreateElement} from 'inferno-create-element'
 import {renderToString as infernoRenderToString} from 'inferno-server'
 
 // Our library!
-import {compose, withState, mapProps, setNodeName} from './index'
-
-// Assertion library
-import chai from 'chai'
-const expect = chai.expect
+import {compose, withState, mapProps, setNodeName} from './dist/classless-component'
 
 const objWithRender = (hyperscript) => ({
   render () {
@@ -26,148 +22,140 @@ const objWithRender = (hyperscript) => ({
   }
 })
 
-describe('compose', () => {
-  it('Should be a function', () => {
-    expect(compose).to.be.a('function')
-  })
-
-  it('Should create a Preact component', () => {
-    const Test = compose(Component, h, objWithRender(h))
-    const output = render(h(Test, {test: 56}))
-    expect(output).to.equal('<div>Hello world</div>')
-  })
-
-  it('Should create a React component', () => {
-    const Test = compose(React.Component, React.createElement, objWithRender(React.createElement))
-    const renderer = ReactTestRenderer.create(React.createElement(Test, {}))
-    const output = renderer.toJSON()
-    expect(output.type).to.equal('div')
-    expect(output.children[0]).to.equal('Hello world')
-  })
-
-  it('Should create an Inferno component', () => {
-    const Test = compose(InfernoComponent, infernoCreateElement, objWithRender(infernoCreateElement))
-    const output = infernoRenderToString(infernoCreateElement(Test))
-    expect(output).to.equal('<div>Hello world</div>')
-  })
-
-  it('Should pass state as props to PFC', () => {
-    const Test = compose(Component, h,
-      {state: {stateNum: 2}},
-      {render ({stateNum, propNum}) {
-        return h('div', null, 'Hello ' + (stateNum * propNum))
-      }}
-    )
-    const output = render(h(Test, {propNum: 4}))
-    expect(output).to.equal('<div>Hello 8</div>')
-  })
-
-  it('Should pass arbitrary functions as props to PFC', () => {
-    const Test = compose(Component, h, {
-      someFunction () {
-        return 8
-      },
-      render ({someFunction}) {
-        return h('div', null, 'Hello ' + someFunction())
-      }
-    })
-    const output = render(h(Test))
-    expect(output).to.equal('<div>Hello 8</div>')
-  })
+test('compose should be a function', (t) => {
+  t.is(typeof compose, 'function')
 })
 
-describe('withState', () => {
-  it('Should set state prop and pass to render', () => {
-    const Counter = compose(Component, h,
-      withState('counter', 'setCounter', 0),
-      {render ({counter, setCounter}) {
-        setCounter(counter + 1)
+test('compose should create a Preact component', (t) => {
+  const Test = compose(Component, h, objWithRender(h))
+  const output = render(h(Test, {test: 56}))
+  t.is(output, '<div>Hello world</div>')
+})
+
+test('compose should create a React component', (t) => {
+  const Test = compose(React.Component, React.createElement, objWithRender(React.createElement))
+  const renderer = ReactTestRenderer.create(React.createElement(Test, {}))
+  const output = renderer.toJSON()
+  t.is(output.type, 'div')
+  t.is(output.children[0], 'Hello world')
+})
+
+test('compose should create an Inferno component', (t) => {
+  const Test = compose(InfernoComponent, infernoCreateElement, objWithRender(infernoCreateElement))
+  const output = infernoRenderToString(infernoCreateElement(Test))
+  t.is(output, '<div>Hello world</div>')
+})
+
+test('compose should pass state as props to PFC', (t) => {
+  const Test = compose(Component, h,
+    {state: {stateNum: 2}},
+    {render ({stateNum, propNum}) {
+      return h('div', null, 'Hello ' + (stateNum * propNum))
+    }}
+  )
+  const output = render(h(Test, {propNum: 4}))
+  t.is(output, '<div>Hello 8</div>')
+})
+
+test('compose should pass arbitrary functions as props to PFC', (t) => {
+  const Test = compose(Component, h, {
+    someFunction () {
+      return 8
+    },
+    render ({someFunction}) {
+      return h('div', null, 'Hello ' + someFunction())
+    }
+  })
+  const output = render(h(Test))
+  t.is(output, '<div>Hello 8</div>')
+})
+
+test('withState should set state prop and pass to render', (t) => {
+  const Counter = compose(Component, h,
+    withState('counter', 'setCounter', 0),
+    {render ({counter, setCounter}) {
+      setCounter(counter + 1)
+      return h('div', null, [
+        'Count: ' + counter,
+        h('button', {onClick: (t) => setCounter(n => n + 1)}, 'Increment'),
+        h('button', {onClick: (t) => setCounter(n => n - 1)}, 'Decrement')
+      ])
+    }}
+  )
+  const output = render(h(Counter))
+  t.is(output, '<div>Count: 0<button>Increment</button><button>Decrement</button></div>')
+})
+
+test('withState should pass props to initialValue function', (t) => {
+  const Counter = compose(Component, h,
+    withState('counter', 'setCounter', ({counter}) => counter * counter),
+    {
+      componentWillMount () {
+        this.setState({counter: this.state.counter * this.state.counter})
+      },
+      render ({counter, setCounter}) {
         return h('div', null, [
           'Count: ' + counter,
-          h('button', {onClick: () => setCounter(n => n + 1)}, 'Increment'),
-          h('button', {onClick: () => setCounter(n => n - 1)}, 'Decrement')
+          h('button', {onClick: (t) => setCounter(n => n + 1)}, 'Increment'),
+          h('button', {onClick: (t) => setCounter(n => n - 1)}, 'Decrement')
         ])
-      }}
-    )
-    const output = render(h(Counter))
-    expect(output).to.equal('<div>Count: 0<button>Increment</button><button>Decrement</button></div>')
-  })
-
-  it('Should pass props to initialValue function', () => {
-    const Counter = compose(Component, h,
-      withState('counter', 'setCounter', ({counter}) => counter * counter),
-      {
-        componentWillMount () {
-          this.setState({counter: this.state.counter * this.state.counter})
-        },
-        render ({counter, setCounter}) {
-          return h('div', null, [
-            'Count: ' + counter,
-            h('button', {onClick: () => setCounter(n => n + 1)}, 'Increment'),
-            h('button', {onClick: () => setCounter(n => n - 1)}, 'Decrement')
-          ])
-        }
       }
-    )
-    const output = render(h(Counter, {counter: 2}))
-    expect(output).to.equal('<div>Count: 16<button>Increment</button><button>Decrement</button></div>')
-  })
-
-  it('Should work with React', () => {
-    const Counter = compose(React.Component, React.createElement,
-      withState('counter', 'setCounter', ({counter}) => counter),
-      {render ({counter, setCounter}) {
-        return React.createElement('div', null, ['Count: ' + counter])
-      }}
-    )
-    const renderer = ReactTestRenderer.create(
-      React.createElement(Counter, {counter: 5}))
-    const output = renderer.toJSON()
-    expect(output.type).to.equal('div')
-    expect(output.children[0]).to.equal('Count: 5')
-  })
+    }
+  )
+  const output = render(h(Counter, {counter: 2}))
+  t.is(output, '<div>Count: 16<button>Increment</button><button>Decrement</button></div>')
 })
 
-describe('mapProps', () => {
-  it('Should work with Preact', () => {
-    const List = compose(Component, h,
-      mapProps(({nums}) => ({nums: nums.map((n) => n + 1)})),
-      {render ({nums}) {
-        return h('div', null, [
-          'List: ' + (nums && nums.join(', '))
-        ])
-      }}
-    )
-    const output = render(h(List, {nums: [1, 2, 3, 4, 5]}))
-    expect(output).to.equal('<div>List: 2, 3, 4, 5, 6</div>')
-  })
-
-  it('Should work with React', () => {
-    const List = compose(React.Component, React.createElement,
-      mapProps((props) => ({nums: props.nums ? props.nums.map((n) => n + 1) : []})),
-      {render ({nums}) {
-        return React.createElement('div', null, [
-          'List: ' + (nums && nums.join(', '))
-        ])
-      }}
-    )
-    const renderer = ReactTestRenderer.create(
-      React.createElement(List, {nums: [1, 2, 3, 4, 5]}))
-    const output = renderer.toJSON()
-    expect(output.type).to.equal('div')
-    expect(output.children[0]).to.equal('List: 2, 3, 4, 5, 6')
-  })
+test('withState should work with React', (t) => {
+  const Counter = compose(React.Component, React.createElement,
+    withState('counter', 'setCounter', ({counter}) => counter),
+    {render ({counter, setCounter}) {
+      return React.createElement('div', null, ['Count: ' + counter])
+    }}
+  )
+  const renderer = ReactTestRenderer.create(
+    React.createElement(Counter, {counter: 5}))
+  const output = renderer.toJSON()
+  t.is(output.type, 'div')
+  t.is(output.children[0], 'Count: 5')
 })
 
-describe('setNodeName', () => {
-  it('Should set nodeName to not be "hoc"', () => {
-    const List = compose(Component, h,
-      setNodeName('CoolComponent'),
-      {render () {
-        return h('div', null, ['Cool!'])
-      }}
-    )
-    const comp = h(List, {nums: [1, 2, 3, 4, 5]})
-    expect(comp.nodeName.name).to.equal('CoolComponent')
-  })
+test('mapProps should work with Preact', (t) => {
+  const List = compose(Component, h,
+    mapProps(({nums}) => ({nums: nums.map((n) => n + 1)})),
+    {render ({nums}) {
+      return h('div', null, [
+        'List: ' + (nums && nums.join(', '))
+      ])
+    }}
+  )
+  const output = render(h(List, {nums: [1, 2, 3, 4, 5]}))
+  t.is(output, '<div>List: 2, 3, 4, 5, 6</div>')
+})
+
+test('mapProps should work with React', (t) => {
+  const List = compose(React.Component, React.createElement,
+    mapProps((props) => ({nums: props.nums ? props.nums.map((n) => n + 1) : []})),
+    {render ({nums}) {
+      return React.createElement('div', null, [
+        'List: ' + (nums && nums.join(', '))
+      ])
+    }}
+  )
+  const renderer = ReactTestRenderer.create(
+    React.createElement(List, {nums: [1, 2, 3, 4, 5]}))
+  const output = renderer.toJSON()
+  t.is(output.type, 'div')
+  t.is(output.children[0], 'List: 2, 3, 4, 5, 6')
+})
+
+test('setNodeName should set nodeName to not be "hoc"', (t) => {
+  const List = compose(Component, h,
+    setNodeName('CoolComponent'),
+    {render () {
+      return h('div', null, ['Cool!'])
+    }}
+  )
+  const comp = h(List, {nums: [1, 2, 3, 4, 5]})
+  t.is(comp.nodeName.name, 'CoolComponent')
 })
